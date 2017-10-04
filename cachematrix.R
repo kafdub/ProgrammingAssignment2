2569aad9ba879af988f3e5810a90bf4917e9219f
Kafil Uddin Ahmad <32220817+kafdub@users.noreply.github.com>
2017-10-04 18:52:42 +0200

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,41 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
-}
-
-
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
-}
+ makeCacheMatrix <- function(x = matrix()) {
++         inv <- NULL
++         set <- function(y) {
++                 x <<- y
++                 inv <<- NULL
++         }
++         get <- function() x
++         setInverse <- function(inverse) inv <<- inverse
++         getInverse <- function() inv
++         list(set = set,
++              get = get,
++              setInverse = setInverse,
++              getInverse = getInverse)
++ }
+> 
+> cacheSolve <- function(x, ...) {
++         ## Return a matrix that is the inverse of 'x'
++         inv <- x$getInverse()
++         if (!is.null(inv)) {
++                 message("getting cached data")
++                 return(inv)
++         }
++         mat <- x$get()
++         inv <- solve(mat, ...)
++         x$setInverse(inv)
++         inv
++ }
+> m <- makeCacheMatrix(matrix(1:5, 2, 2))
+> m$get()
+     [,1] [,2]
+[1,]    1    3
+[2,]    2    4
+Warning message:
+In matrix(1:5, 2, 2) :
+  data length [5] is not a sub-multiple or multiple of the number of rows [2]
+> m$getInverse()
+NULL
+> cacheSolve(m)
+     [,1] [,2]
+[1,]   -2  1.5
+[2,]    1 -0.5
